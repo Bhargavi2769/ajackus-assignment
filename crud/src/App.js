589effb608182cc1f2d5cbbe3cf36a6c7d90bc94
Bhargavi2769 <62629611+Bhargavi2769@users.noreply.github.com>
2025-01-28 08:54:02 +0530

    Update App.js

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UserForm from "./components/userForm/userForm";
 import UserList from "./components/userList/userList";
-// import UserLists from "./components/userList/userLists";
 
 const App = () => {
   return (
